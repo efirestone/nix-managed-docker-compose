89788c7aa31447c060a82f7fab3da0ad7346aa0d
netpleb <netpleb@users.noreply.github.com>
2025-01-18 13:46:46 -0800

    added trivial module test which can be run by `nix flake check --all-systems --print-build-logs`

--- flake.nix
+++ flake.nix
@@ -51,5 +51,51 @@
 
       # the following line with // will concat the attrs
     }) // { hello-world = self.nixosConfigurations.x86_64-linux.hello-world; };
+
+    checks = forEachSystem (system: {
+      moduleTest = nixpkgs.legacyPackages.${system}.testers.runNixOSTest {
+        name = "moduleTest";
+        nodes.machine1 = {
+          imports = [ self.outputs.nixosModules.managed-docker-compose ];
+
+          # enable our custom module
+          services.managed-docker-compose.enable = true;
+
+          # our module requires these things (maybe it should enable them itself too?)
+          virtualisation.docker.enable = true;
+          environment.systemPackages = with nixpkgs.legacyPackages.${system}; [ 
+            docker
+            docker-compose
+          ];
+
+          # A default user able to use sudo
+          users.users.guest = {
+            isNormalUser = true;
+            home = "/home/guest";
+            extraGroups = [ "wheel" "docker" ];
+            initialPassword = "guest";
+          };
+
+          security.sudo.wheelNeedsPassword = false;
+
+          # got a small docker compose from here:
+          #  https://stackoverflow.com/questions/71719908/what-is-the-smallest-image-that-can-be-used-to-leave-docker-compose-running-inde
+          environment.etc."docker-compose/hello/docker-compose.yaml".text = 
+            ''
+            version: '3.9'
+            services:
+              myservice:
+                image: registry.hub.docker.com/alpine:latest
+                command: tail -f /dev/null
+            '';
+        };
+        # TODO: need to make this into a non-trivial test!
+        #       see: https://vtimofeenko.com/posts/practical-nix-flake-anatomy-a-guided-tour-of-flake.nix/#checks
+        testScript = ''
+          machine.wait_for_unit("default.target")
+          assert "hello" == "hello"
+        '';
+      };
+    });
   };
 }
